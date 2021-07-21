7931e232a883190b998ad9ea2a975b905d223c34
khjde <khjde1207@gmail.com>
2021-07-21 16:47:23 +0900

    support web (developing)

--- example/README.md
+++ example/README.md
@@ -15,35 +15,17 @@ For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
-## Common Issues
-
-Tessearct may crash with SIG: 9. It is resolved maybe by `flutter clean` or downgrade NDK. If it reports class not found for TessBaseAPI, please add tesseract4android-release following the example.
-
-For the following error,
-
+# run web app 
 ```
-> Direct local .aar file dependencies are not supported when building an AAR. The resulting AAR would be broken because the classes and Android resources from any local .aar file dependencies would not be packaged in the resulting AAR. Previous versions of the Android Gradle Plugin produce broken AARs in this case too (despite not throwing this error). The following direct local .aar file dependencies of the :flutter_tesseract_ocr project caused this error: C:\dev\git\QuanotesX\packages\flutter_tesseract_ocr\android\libs\tesseract4android-release.aar
+flutter channel master
+flutter upgrade
+flutter pub get 
+flutter run -d chrome
 ```
-
-Add compileOnly and comment implementation, like:
-
+# run android or ios 
 ```
-dependencies {
-    compileOnly files('libs/tesseract4android-release.aar')
-    //implementation fileTree(include: '*.aar', dir: 'libs')
-}
+flutter pub get 
+flutter run 
 ```
 
-For error
 
-```
-> No version of NDK matched the requested version 21.0.6113669. Versions available locally: 20.0.5594570
-```
-
-Just install the requested version.
-
-A verified combination is:
-
-- classpath 'com.android.tools.build:gradle:3.6.2'
-- NdkVersion "20.0.5594570"
-- aar attached
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart' show ByteData, rootBundle;
 import 'dart:io';
 import 'package:image/image.dart' as im;
 import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(' >>>> ${kIsWeb}');
     return MaterialApp(
       title: 'Tesseract Demo',
       theme: ThemeData(
@@ -40,8 +42,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _ocrText = '';
   String _ocrHocr = '';
-  String path =
-      "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FqCviW%2FbtqGWTUaYLo%2FwD3ZE6r3ARZqi4MkUbcGm0%2Fimg.png";
+  Map<String, String> tessimgs = {
+    "kor":
+        "https://raw.githubusercontent.com/khjde1207/tesseract_ocr/master/example/assets/test1.png",
+    "en": "https://tesseract.projectnaptha.com/img/eng_bw.png",
+    "ch_sim": "https://tesseract.projectnaptha.com/img/chi_sim.png",
+    "ru": "https://tesseract.projectnaptha.com/img/rus.png",
+  };
+  var LangList = ["kor", "eng", "deu", "chi_sim"];
+  var selectList = ["eng"];
+  String path = "";
+  // "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FqCviW%2FbtqGWTUaYLo%2FwD3ZE6r3ARZqi4MkUbcGm0%2Fimg.png";
+  var urlEditController = TextEditingController();
 
   Future<void> writeToFile(ByteData data, String path) {
     final buffer = data.buffer;
@@ -49,55 +61,67 @@ class _MyHomePageState extends State<MyHomePage> {
         buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
   }
 
-  void _ocr() async {
+  void runFilePiker() async {
+    final pickedFile =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+  }
+
+  void _ocr(url) async {
     // final filename = 'test1.png';
     // var bytes = await rootBundle.load("assets/test1.png");
     // String dir = (await getApplicationDocumentsDirectory()).path;
     // writeToFile(bytes, '$dir/$filename');
-    final pickedFile =
-        await ImagePicker().getImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      // ---- dynamic add traineddata ---- ▼
-      // https://github.com/tesseract-ocr/tessdata/raw/master/dan_frak.traineddata
-      // download
-      // String newTessDataFile = "deu.traineddata";
-      // Directory d = Directory(await TesseractOcr.getTessdataPath());
-      // d.list().forEach((event) {
-      //   print(event);
-      // });
-      // File('${d.path}/${newTessDataFile}').writeAsBytes([Uint8List bytes]);
-      // ---- dynamic add traineddata ---- ▲
-
-      _ocrText = await FlutterTesseractOcr.extractText(pickedFile.path,
-          language: 'kor',
-          args: {
-            "preserve_interword_spaces": "1",
-          });
-      setState(() {});
-
-      im.Image image = im.decodeImage(File(pickedFile.path).readAsBytesSync())!;
-      image = im.adjustColor(
-        image.clone(),
-        // blacks: 100,
-        // amount: 1,
-        // saturation: 0,
-        gamma: 5,
-        // exposure: 0.7,
-      );
-
-      File('${pickedFile.path}_op.jpg')..writeAsBytesSync(im.encodeJpg(image));
-      path = '${pickedFile.path}_op.jpg';
-      setState(() {});
-      _ocrHocr = await FlutterTesseractOcr.extractText(
-          '${pickedFile.path}_op.jpg',
-          language: 'kor',
-          args: {
-            "psm": "4",
-            "preserve_interword_spaces": "1",
-          });
-      print(_ocrHocr.split('   ').length);
-      setState(() {});
+    // final pickedFile =
+    //     await ImagePicker().getImage(source: ImageSource.gallery);
+    // if (pickedFile != null) {
+    // ---- dynamic add traineddata ---- ▼
+    // https://github.com/tesseract-ocr/tessdata/raw/master/dan_frak.traineddata
+    // download
+    // String newTessDataFile = "deu.traineddata";
+    // Directory d = Directory(await TesseractOcr.getTessdataPath());
+    // d.list().forEach((event) {
+    //   print(event);
+    // });
+    // File('${d.path}/${newTessDataFile}').writeAsBytes([Uint8List bytes]);
+    // ---- dynamic add traineddata ---- ▲
+    if (selectList.length <= 0) {
+      print("Please select language");
+      return;
     }
+    var langs = selectList.join("+");
+
+    path = url;
+    setState(() {});
+    _ocrText =
+        await FlutterTesseractOcr.extractText(url, language: langs, args: {
+      "preserve_interword_spaces": "1",
+    });
+    print(_ocrText);
+    setState(() {});
+
+    // im.Image image = im.decodeImage(File(pickedFile.path).readAsBytesSync())!;
+    // image = im.adjustColor(
+    //   image.clone(),
+    //   // blacks: 100,
+    //   // amount: 1,
+    //   // saturation: 0,
+    //   gamma: 5,
+    //   // exposure: 0.7,
+    // );
+
+    // File('${pickedFile.path}_op.jpg')..writeAsBytesSync(im.encodeJpg(image));
+    // path = '${pickedFile.path}_op.jpg';
+    // setState(() {});
+    // _ocrHocr = await FlutterTesseractOcr.extractText(
+    //     '${pickedFile.path}_op.jpg',
+    //     language: 'kor',
+    //     args: {
+    //       "psm": "4",
+    //       "preserve_interword_spaces": "1",
+    //     });
+    // print(_ocrHocr.split('   ').length);
+    // setState(() {});
+    // }
   }
 
   @override
@@ -106,49 +130,191 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: ListView(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            // Text(
-            //   'OCR result:',
-            // ),
-            ElevatedButton(
-                onPressed: () async {
-                  print(await FlutterTesseractOcr.extractText(path,
-                      language: 'kor',
-                      args: {
-                        "psm": "4",
-                        "preserve_interword_spaces": "1",
-                      }));
-                },
-                child: Text("test")),
-
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                Container(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return SimpleDialog(
+                                title: const Text('Select Url'),
+                                children: tessimgs
+                                    .map((key, value) {
+                                      return MapEntry(
+                                          key,
+                                          SimpleDialogOption(
+                                              onPressed: () {
+                                                urlEditController.text = value;
+                                                setState(() {});
+                                                Navigator.pop(context);
+                                              },
+                                              child: Row(
+                                                children: [
+                                                  Text(key),
+                                                  Text(" : "),
+                                                  Flexible(child: Text(value)),
+                                                ],
+                                              )));
+                                    })
+                                    .values
+                                    .toList(),
+                              );
+                            });
+                      },
+                      child: Text("urls")),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'input image url',
+                    ),
+                    controller: urlEditController,
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      _ocr(urlEditController.text);
+                    },
+                    child: Text("Run")),
+              ],
+            ),
+            Row(
+              children: [
+                ...LangList.map((e) {
+                  return Row(children: [
+                    Checkbox(
+                        value: selectList.indexOf(e) >= 0,
+                        onChanged: (v) {
+                          if (selectList.indexOf(e) < 0) {
+                            selectList.add(e);
+                          } else {
+                            selectList.remove(e);
+                          }
+                          setState(() {});
+                        }),
+                    Text(e)
+                  ]);
+                }).toList(),
+              ],
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                path.length <= 0
+                    ? Container()
+                    : path.indexOf("http") >= 0
+                        ? Image.network(path)
+                        : Image.file(File(path)),
                 Text(
                   '$_ocrText',
                   // style: Theme.of(context).textTheme.headline4,
                 ),
-                Text(
-                  '$_ocrHocr',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
               ],
-            ),
-            Divider(),
-            Image.network(path),
-
-            // Image.file(File(path)),
+            ))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _ocr,
-        tooltip: 'OCR',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // Center(
+      //   child: ListView(
+      //     // mainAxisAlignment: MainAxisAlignment.start,
+      //     children: <Widget>[
+      //       Row(
+      //         children: [
+      //           Container(
+      //             child: ElevatedButton(
+      //                 onPressed: () {
+      //                   showDialog(
+      //                       context: context,
+      //                       builder: (BuildContext context) {
+      //                         return SimpleDialog(
+      //                           title: const Text('Select Url'),
+      //                           children: tessimgs
+      //                               .map((key, value) {
+      //                                 return MapEntry(
+      //                                     key,
+      //                                     SimpleDialogOption(
+      //                                         onPressed: () {
+      //                                           urlEditController.text = value;
+      //                                           setState(() {});
+      //                                           Navigator.pop(context);
+      //                                         },
+      //                                         child: Row(
+      //                                           children: [
+      //                                             Text(key),
+      //                                             Text(" : "),
+      //                                             Flexible(child: Text(value)),
+      //                                           ],
+      //                                         )));
+      //                               })
+      //                               .values
+      //                               .toList(),
+      //                         );
+      //                       });
+      //                 },
+      //                 child: Text("urls")),
+      //           ),
+      //           Expanded(
+      //             child: TextField(
+      //               decoration: InputDecoration(
+      //                 border: OutlineInputBorder(),
+      //                 labelText: 'input image url',
+      //               ),
+      //               controller: urlEditController,
+      //             ),
+      //           ),
+      //           ElevatedButton(onPressed: () {}, child: Text("Run")),
+      //         ],
+      //       ),
+      //       // ElevatedButton(
+      //       //     onPressed: () async {
+      //       //       print(await FlutterTesseractOcr.extractText(path,
+      //       //           language: 'kor',
+      //       //           args: {
+      //       //             "psm": "4",
+      //       //             "preserve_interword_spaces": "1",
+      //       //           }));
+      //       //     },
+      //       //     child: Text("test")),
+
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //         children: [
+      //           Text(
+      //             '$_ocrText',
+      //             // style: Theme.of(context).textTheme.headline4,
+      //           ),
+      //           Text(
+      //             '$_ocrHocr',
+      //             style: Theme.of(context).textTheme.bodyText1,
+      //           ),
+      //         ],
+      //       ),
+      //       Divider(),
+      //       Image.network(path),
+
+      //       // Image.file(File(path)),
+      //     ],
+      //   ),
+      // ),
+
+      floatingActionButton: kIsWeb
+          ? Container()
+          : FloatingActionButton(
+              onPressed: () {
+                runFilePiker();
+                // _ocr("");
+              },
+              tooltip: 'OCR',
+              child: Icon(Icons.add),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
