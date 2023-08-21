aa24d3d9c832de104eb6568fee9392c254f7807e
yqzhouluyao <yqzhouluyao@gmail.com>
2023-08-22 01:52:18 +0800

    修改OCR插件的实现

--- lib/android_ios.dart
+++ lib/android_ios.dart
@@ -1,28 +1,21 @@
 // part of flutter_tesseract_ocr;
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 
 class FlutterTesseractOcr {
-  static const String TESS_DATA_CONFIG = 'assets/tessdata_config.json';
-  static const String TESS_DATA_PATH = 'assets/tessdata';
   static const MethodChannel _channel =
       const MethodChannel('flutter_tesseract_ocr');
 
-  /// image to  text
-  ///```
-  /// String _ocrText = await FlutterTesseractOcr.extractText(url, language: langs, args: {
-  ///    "preserve_interword_spaces": "1",});
-  ///```
+  /// image to text
   static Future<String> extractText(String imagePath,
       {String? language, Map? args}) async {
     assert(await File(imagePath).exists(), true);
-    final String tessData = await _loadTessData();
+
+    final String tessData = await getTessdataPath();
     final String extractText =
         await _channel.invokeMethod('extractText', <String, dynamic>{
       'imagePath': imagePath,
@@ -33,15 +26,12 @@ class FlutterTesseractOcr {
     return extractText;
   }
 
-  /// image to  html text(hocr)
-  ///```
-  /// String _ocrHocr = await FlutterTesseractOcr.extractText(url, language: langs, args: {
-  ///    "preserve_interword_spaces": "1",});
-  ///```
+  /// image to html text (hocr)
   static Future<String> extractHocr(String imagePath,
       {String? language, Map? args}) async {
     assert(await File(imagePath).exists(), true);
-    final String tessData = await _loadTessData();
+
+    final String tessData = await getTessdataPath();
     final String extractText =
         await _channel.invokeMethod('extractHocr', <String, dynamic>{
       'imagePath': imagePath,
@@ -53,39 +43,9 @@ class FlutterTesseractOcr {
   }
 
   /// getTessdataPath
-  ///```
-  /// print(await FlutterTesseractOcr.getTessdataPath())
-  ///```
   static Future<String> getTessdataPath() async {
     final Directory appDirectory = await getApplicationDocumentsDirectory();
     final String tessdataDirectory = join(appDirectory.path, 'tessdata');
     return tessdataDirectory;
-  }
-
-  static Future<String> _loadTessData() async {
-    final Directory appDirectory = await getApplicationDocumentsDirectory();
-    final String tessdataDirectory = join(appDirectory.path, 'tessdata');
-
-    if (!await Directory(tessdataDirectory).exists()) {
-      await Directory(tessdataDirectory).create();
-    }
-    await _copyTessDataToAppDocumentsDirectory(tessdataDirectory);
-    return appDirectory.path;
-  }
-
-  static Future _copyTessDataToAppDocumentsDirectory(
-      String tessdataDirectory) async {
-    final String config = await rootBundle.loadString(TESS_DATA_CONFIG);
-    Map<String, dynamic> files = jsonDecode(config);
-    for (var file in files["files"]) {
-      if (!await File('$tessdataDirectory/$file').exists()) {
-        final ByteData data = await rootBundle.load('$TESS_DATA_PATH/$file');
-        final Uint8List bytes = data.buffer.asUint8List(
-          data.offsetInBytes,
-          data.lengthInBytes,
-        );
-        await File('$tessdataDirectory/$file').writeAsBytes(bytes);
-      }
-    }
   }
 }
